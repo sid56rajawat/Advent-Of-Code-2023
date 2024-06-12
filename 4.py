bf64dfe6fa222e4971ed3e84f1701863ed0c4bc2
Shivam Rajawat <90323915+sid56rajawat@users.noreply.github.com>
2024-06-12 21:25:01 +0530

    Day 4 Part 2

--- 4.py
+++ 4.py
@@ -1,13 +1,10 @@
 import sys
 table = open(sys.argv[1]).read().strip()
 
-# for each card
-# make set of Winning numbers
-# then iterate through our numbers and increment count
-# if our number in winning numbers
-# at the end: ans += cnt == 0 ? 0 : 2 ** (cnt-1)
-ans = 0
-for card in table.split('\n'):
+cards = table.split('\n')
+matchingNumbersCount = [0] * len(cards)
+for i in range(0,len(cards)):
+    card = cards[i]
     wn, on = card.split('|')
     wn = wn.split(':')[1]
     winningNumbers = set(wn.split(' '))
@@ -16,6 +13,12 @@ for card in table.split('\n'):
     cnt = 0
     for num in ourNumbers:
         if(num in winningNumbers): cnt+=1
-    ans += 0 if cnt == 0 else 2 ** (cnt-1)
-    
-print(ans)
+    matchingNumbersCount[i] += cnt
+
+instances = [1] * len(cards)
+for i in range(0,len(cards)):
+    mnc = matchingNumbersCount[i]
+    for j in range(1,mnc+1):
+        if(i+j < len(cards)): instances[i+j] += instances[i]
+
+print(sum(instances))
